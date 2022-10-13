eab37dc48b6d1b5654cac1ea46d60cba5afd12a0
sikiz <sikiz@mobiquityinc.com>
2022-10-13 19:30:28 +0200

    refactor menu access class

--- src/main/java/com/c/refactoring/menuexamples/MenuAccess.java
+++ src/main/java/com/c/refactoring/menuexamples/MenuAccess.java
@@ -1,34 +1,33 @@
 package com.c.refactoring.menuexamples;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MenuAccess {
 
     public void setAuthorizationsInEachMenus(
             List<MenuItem> menuItemsList, Role[] roles) {
-        for (int i = 0; i < menuItemsList.size(); i++) {
-            MenuItem menuItem = menuItemsList.get(i);
-            if (roles != null) {
-                for (int j = 0; j < roles.length; j++) {
-                    if (roles[j].getName().equals(menuItem
-                            .getReadAccessRole())
-                            && !Constants.WRITE.equals(
-                                    menuItem
-                                            .getAccess())) {
-                        menuItem.setAccess(Constants.READ);
-                        menuItem.setVisible(true);
-                    } else if (roles[j].getName().equals(
-                            menuItem
-                                    .getWriteAccessRole())) {
-                        menuItem.setAccess(Constants.WRITE);
-                        menuItem.setVisible(true);
-                    }
-                }
+        if(roles == null)
+            return;
 
+        menuItemsList.forEach(menuItem ->  {
+            if(doesUserHaveTheRole(roles, menuItem.getReadAccessRole())) {
+                setMenuItemAccess(menuItem, Constants.READ);
             }
 
-        }
+            if(doesUserHaveTheRole(roles, menuItem.getWriteAccessRole())){
+                setMenuItemAccess(menuItem, Constants.WRITE);
+            }
+        });
+    }
 
+    private boolean doesUserHaveTheRole(Role[] roles, String menuItem) {
+        return Arrays.stream(roles).anyMatch(role -> role.getName().equals(menuItem));
+    }
+
+    private void setMenuItemAccess(MenuItem menuItem, String access) {
+        menuItem.setAccess(access);
+        menuItem.setVisible(true);
     }
 
 }
