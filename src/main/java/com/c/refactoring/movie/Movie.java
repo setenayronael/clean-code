c238afe2abe2e31fb91ebdab4e0b522291da020e
sikiz <sikiz@mobiquityinc.com>
2022-10-13 17:18:00 +0200

    refactor Movie isValidRating with regex match

--- src/main/java/com/c/refactoring/movie/Movie.java
+++ src/main/java/com/c/refactoring/movie/Movie.java
@@ -1,7 +1,5 @@
 package com.c.refactoring.movie;
 
-import com.c.refactoring.StringUtils;
-
 public class Movie {
 
     String rating;
@@ -15,23 +13,9 @@ public class Movie {
         return rating;
     }
 
-    /*Axx or By
-    Where x represents any digit between 0 and 9, and y represents 
-    any digit between 1 and 4*/
     public boolean isValidRating() {
         if (this.getRating() != null) {
-            if (this.getRating().substring(0, 1).equalsIgnoreCase("B")
-                    && this.getRating().length() == 2) {
-                if (StringUtils.isNumeric(this.getRating().substring(1, 2))
-                        && Integer.parseInt(this.getRating().substring(1, 2)) > 0
-                        && Integer.parseInt(this.getRating().substring(1, 2)) < 5)
-                    return true;
-
-            } else if (this.getRating().substring(0, 1).equalsIgnoreCase("A")
-                    && this.getRating().length() == 3
-                    && StringUtils.isNumeric(this.getRating().substring(1, 3)))
-                return true;
-
+            return this.getRating().matches("(A[0-9]{2}|B[1-4])");
         }
         return false;
     }
